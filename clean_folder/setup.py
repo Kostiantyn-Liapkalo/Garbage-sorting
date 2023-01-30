bdd71c9b0a4bd92681854f1ea038eb0b75d0c892
Kostiantyn-Liapkalo <lynx87@ukr.net>
2023-01-30 22:25:57 +0200

    debug setup

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -11,5 +11,5 @@ setup(
     packages=find_namespace_packages(),
     include_package_data=True,
     entry_points={'console_scripts': [
-        'startclean=clean_folder.sort_files:run']}
+        'clean-folder=clean_folder.sort_files:run']}
 )
